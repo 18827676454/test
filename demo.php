1e750bfc911766eda467fac50bed6e75462a5af1
Randy <284431745@qq.com>
2019-06-30 22:50:55 +0800

    test

---
+++ demo.php
@@ -0,0 +1,5 @@
+<?php
+require "./vendor/autoload.php";
+use randy\test\Tools;
+$res =  Tools::getDay();
+var_dump($res);
---
+++ src/randy/test/Tools.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ +----------------------------------------------------------------------
+ + Title        : Tools 控制器
+ + Author       : Randy_chen
+ + Version      : V1.0.0.1
+ + Initial-Time : 2019/6/30 22:24
+ + Last-time    : 2019/6/30 22:24+ 86187
+ + Desc         : Tools
+ +----------------------------------------------------------------------
+*/
+
+namespace randy\test;
+class Tools
+{
+    public function __construct()
+    {
+    }
+
+    public static function getDay(){
+        echo  11111;
+    }
+}
